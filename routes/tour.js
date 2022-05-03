37b8372bed606788ecd04f49e5341721d5cc73bf
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-05-03 18:52:12 +0530

    Added booking functionality

--- routes/tour.js
+++ routes/tour.js
@@ -4,19 +4,25 @@ const tourList = require("../models/tourList");
 
 //Create a new tour
 
-router.post("/",  async (req, res) => {
-  
-    const newList = new tourList(req.body);
-    try {
-      const savedList = await newList.save();
-      res.status(201).json(savedList);
-    } catch (err) {
-      res.status(500).json(err);
-      console.log(err);
-    }
-   
-  
-}) ;
+router.post("/", async (req, res) => {
+
+  const newList = new tourList(req.body);
+  try {
+    const savedList = await newList.save();
+    const count = await newList.count({ to });
+    if (count > 3) throw new Error("no more tickets for you");
+    else savedList = await newList.save();
+
+    res.status(201).json(savedList);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+
+  // alternative: const count = await posts.count({userId, _id: {$gte: startofDayConvertedToMongoId});
+
+
+});
 
 
 module.exports = router;
