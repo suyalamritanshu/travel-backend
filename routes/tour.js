201e6b4a9e2abaf27645fff64dbe600af5612d69
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-05-03 18:57:28 +0530

    Added booking functionality

--- routes/tour.js
+++ routes/tour.js
@@ -10,7 +10,7 @@ router.post("/", async (req, res) => {
   try {
     const savedList = await newList.save();
     const count = await newList.count({ to });
-    if (count > 2) throw new Error("no more tickets for you");
+    if (count > 2) throw res.status(401).json(savedList);
     else savedList = await newList.save();
 
     res.status(201).json(savedList);
