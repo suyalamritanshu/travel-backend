9d8e82b497cbcf8e697de5462462909bea92851c
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-05-04 08:08:07 +0530

    fixes

--- models/tourList.js
+++ models/tourList.js
@@ -4,6 +4,11 @@ const ListSchema = new mongoose.Schema(
   {
     to: { type: String, required: true },
     date: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+
+
   },
   { timestamps: true }
 );
